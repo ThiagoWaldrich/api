40c03293eafc344f0ce8470af1dc783de9e13176
ThiagoWaldrich <thiagowlf7@gmail.com>
2024-01-25 19:59:52 -0300

    table pacientes added

--- src/main/java/med/voll/api/controller/PacienteController.java
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -1,6 +1,12 @@
 package med.voll.api.controller;
 
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
 import med.voll.api.paciente.DadosCadastroPaciente;
+import med.voll.api.paciente.Paciente;
+import med.voll.api.paciente.PacienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,7 +15,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/pacientes")
 
 public class PacienteController {
-    public void cadastrarPaciente(@RequestBody DadosCadastroPaciente dados){
-        System.out.println(dados);
-    }
+    @Autowired
+    private PacienteRepository pacienteRepository;
+
+
+    @PostMapping
+    @Transactional
+    public void cadastrar(@RequestBody @Valid DadosCadastroPaciente dados){
+        pacienteRepository.save(new Paciente(dados));
+}
 }
